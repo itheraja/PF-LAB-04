b893a8cd8693f4468de0ae8aa3f73fcb1ab2eeb9
Raja Muhammad Taha <rajataha151@gmail.com>
2025-11-28 23:39:33 +0500

    Add ice cream scoop pricing program

---
+++ q2.c
@@ -0,0 +1,17 @@
+/* Q2 - single scoop vs multi-scoop message */
+#include <stdio.h>
+
+int main() {
+    int scoops;
+    const double one_scoop_price = 2.00;
+    printf("How many scoops do you want? ");
+    scanf("%d", &scoops):
+    if (scoops > 1) {
+        printf("Please see our multi-scoop offers.\n");
+    } else if (scoops == 1) {
+        printf("Your total is $%.2f\n", one_scoop_price);
+    } else {
+        printf("Invalid number of scoops.\n");
+    }
+    return 0;
+}
